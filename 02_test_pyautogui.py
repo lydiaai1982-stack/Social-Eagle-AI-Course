12c233cafdceb30c3de53cdb40416b38ef049c01
lydiaai1982-stack <lydia.ai1982@gmail.com>
2025-11-02 14:30:59 +0530

    Fix import and function name typos in test script

--- 02_test_pyautogui.py
+++ 02_test_pyautogui.py
@@ -1,12 +1,13 @@
-import pyautogusi
+import pyautogui
 import time
 
 #give yourself a few seconds to hover over the target
 
 time.sleep (5)
 
-x,y = pyautsogui.position()
+x,y = pyautsogui.positi on()
 
 print (f"mouse is at ({x}, {y})")
+
 
 
