3ff32e36247dabf4c9f85591bb1fb52e2c658eb1
lydiaai1982-stack <lydia.ai1982@gmail.com>
2025-11-02 15:37:51 +0530

    Fix import and variable name typos in test script

--- 02_test_pyautogui.py
+++ 02_test_pyautogui.py
@@ -1,14 +1,1 @@
-import pyssautogui
-import time
-
-#give yourself a few seconds to hover over the target
-
-time.sleep (5)
-
-x,y = pyautsogui.positi on()
-
-print (f"mouse is at ({x}, {y})")
-
-
-
 
